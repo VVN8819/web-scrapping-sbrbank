47f3dd125d8751f968a7104899d385fbdb8716d1
VVN8819 <vitalii8819@gmail.com>
2026-05-09 15:15:19 +0800

    features: Добавил парсер BeautifulSoup и вывод валюты YUAN

--- scripts/currency_scrapping.py
+++ scripts/currency_scrapping.py
@@ -1,10 +1,34 @@
 import requests
+from bs4 import BeautifulSoup
 
-# Сохраняем URL
 url = "https://www.cbr.ru/"
-
-# GET-запрос к серверу
 response = requests.get(url)
-
-# Проверяем статус (200 - "OK")
 print(f'Статус-код: {response.status_code}')
+
+# Добавим парсер из текста страницы
+soup = BeautifulSoup(response.text, 'lxml')
+print(f'Источник: {soup.title.text}')
+
+# ======================= Находим валюту ==============================
+currencies = soup.find_all('div', class_='main-indicator_rate')
+
+# Ищем блок
+for currency in currencies:
+  class_name = currency.find('div', class_='_yuan')
+
+  # Если нашли — вытаскиваем данные
+  if class_name:
+
+    currency_name = class_name.get_text(strip=True) if class_name else "Не найдено"
+
+    # Курсы: ищем ВСЕ div с классом "_left" и "mono-num"
+    class_list = ['_left', 'mono-num']
+    rate_divs = currency.find_all('div', class_=class_list)
+
+    current_rate = rate_divs[0].get_text(strip=True) if len(rate_divs) > 0 else "N/A"
+    previous_rate = rate_divs[1].get_text(strip=True) if len(rate_divs) > 1 else "N/A"
+
+    print(f'\nВалюта: {currency_name}')
+    print(f'- Был вчера: {current_rate}')
+    print(f'- Текущий курс: {previous_rate}')
+
